3bbaa3afccb9f0ec48ace5bb69505bdad6dd9200
mertncu <ymuncu@gmail.com>
2024-03-17 18:19:50 +0300

    post method app

--- app/src/main/java/com/mert/postmethod/data/network/ApiService.kt
+++ app/src/main/java/com/mert/postmethod/data/network/ApiService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("/gateway/AddProduct")
+    @POST("end-point-here")
     suspend fun addProduct(@Body product: Product): ApiResponse
 }
--- app/src/main/java/com/mert/postmethod/data/network/RetrofitClient.kt
+++ app/src/main/java/com/mert/postmethod/data/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://inventorygateway.intalalab.net"
+    private const val BASE_URL = "API-URL"
 
     val apiService: ApiService by lazy {
         val retrofit = Retrofit.Builder()
